a553cf70bf9fe0fd7c9799d644823a4329d54018
Yarek T <yarekt@gmail.com>
2013-10-13 09:45:42 +0100

    Fix the Vote migration case problem

--- app/DoctrineMigrations/Version20131013012244.php
+++ app/DoctrineMigrations/Version20131013012244.php
@@ -15,9 +15,9 @@ class Version20131013012244 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("CREATE TABLE Vote (id INT AUTO_INCREMENT NOT NULL, chore_id INT DEFAULT NULL, user_id INT DEFAULT NULL, positive TINYINT(1) NOT NULL, created DATETIME NOT NULL, INDEX IDX_FA222A5A6C576F80 (chore_id), INDEX IDX_FA222A5AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
-        $this->addSql("ALTER TABLE Vote ADD CONSTRAINT FK_FA222A5A6C576F80 FOREIGN KEY (chore_id) REFERENCES chore (id)");
-        $this->addSql("ALTER TABLE Vote ADD CONSTRAINT FK_FA222A5AA76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)");
+        $this->addSql("CREATE TABLE vote (id INT AUTO_INCREMENT NOT NULL, chore_id INT DEFAULT NULL, user_id INT DEFAULT NULL, positive TINYINT(1) NOT NULL, created DATETIME NOT NULL, INDEX IDX_FA222A5A6C576F80 (chore_id), INDEX IDX_FA222A5AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
+        $this->addSql("ALTER TABLE vote ADD CONSTRAINT FK_FA222A5A6C576F80 FOREIGN KEY (chore_id) REFERENCES chore (id)");
+        $this->addSql("ALTER TABLE vote ADD CONSTRAINT FK_FA222A5AA76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)");
     }
 
     public function down(Schema $schema)
@@ -25,6 +25,6 @@ class Version20131013012244 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("DROP TABLE Vote");
+        $this->addSql("DROP TABLE vote");
     }
 }
