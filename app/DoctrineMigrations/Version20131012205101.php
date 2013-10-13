afb1993aa3abe5deebb4644e67ebb31d008bf2a7
Wayne Crouch <wcrouch@gmail.com>
2013-10-13 01:38:40 +0100

    Fix migrations

--- app/DoctrineMigrations/Version20131012205101.php
+++ app/DoctrineMigrations/Version20131012205101.php
@@ -15,6 +15,7 @@ class Version20131012205101 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
+        $this->addSql("CREATE TABLE habitat (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
         $this->addSql("ALTER TABLE habitat ADD name VARCHAR(100) NOT NULL");
     }
 
